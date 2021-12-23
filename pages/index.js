3ce264d1197150b2e6fb9f2fb31bd7800a32274b
kingjamestt <brandonjames101@gmail.com>
2021-12-23 17:57:03 -0400

    Added dynamic routing for posts

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,6 @@ export default function Home({ articles }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Home Page!</h1>
         <ArticleList articles={articles} />
       </main>
     </div>
